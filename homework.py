689c391a82376e626499591fa4962110fcf7531d
Андрей Цай <skaeris@yandex.ru>
2023-05-18 02:15:51 +0300

    Second try_4

--- homework.py
+++ homework.py
@@ -11,9 +11,9 @@ from telegram.ext import Updater
 
 load_dotenv()
 
-PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN_ID')
-TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN_ID')
-TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID_NUMBER')
+PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 RETRY_PERIOD = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
