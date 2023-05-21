29728781356041ea68f2d8bb7ebc6d28f248b87f
Андрей Цай <skaeris@yandex.ru>
2023-05-21 18:27:45 +0300

    Last try

--- homework.py
+++ homework.py
@@ -26,6 +26,12 @@ HOMEWORK_VERDICTS = {
 }
 
 
+class SendMessageError(Exception):
+    """Ошибка отправки сообщения в Telegram."""
+
+    pass
+
+
 def check_tokens():
     """Функция проверки токена."""
     return all([PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID])
@@ -42,17 +48,15 @@ def send_message(bot, message):
 
 def get_api_answer(timestamp):
     """Делает запрос к единственному эндпоинту API-сервиса."""
-    timestamp = int(time.time())
+    timestamp = timestamp or int(time.time())
     params = {'from_date': timestamp}
     try:
-        response = requests.get(
-            dict(url=ENDPOINT, headers=HEADERS, params=params))
+        response = requests.get(url=ENDPOINT, headers=HEADERS, params=params)
     except requests.exceptions.RequestException as error:
         raise telegram.TelegramError(
             f'Ошибка подключения {error}')
-    response_status = response.status_code
-    if response_status != 200:
-        raise KeyError(f'Endpoint не доступен {response_status}')
+    if response.status_code != 200:
+        raise KeyError(f'Endpoint не доступен {response.status_code}')
     return response.json()
 
 
@@ -72,18 +76,17 @@ def check_response(response):
 def parse_status(homework):
     """Извлекает статус работы из информации о конкретной домашней работе."""
     homework_name = homework.get('homework_name')
-    if not homework_name:
+    if 'homework_name' not in homework:
         raise KeyError('Ответ API не содержит ключа "homework_name"')
     homework_status = homework.get('status')
-    if not homework_status:
+    if 'status' not in homework:
         raise KeyError('Ответ API не содержит ключа "homework_status"')
     verdict = HOMEWORK_VERDICTS.get(homework_status)
     if not verdict:
         raise KeyError(f'Неизвестный статус "{homework_status}" '
                        f'у работы "{homework_name}"')
     verdict = HOMEWORK_VERDICTS[homework_status]
-    message = f'Изменился статус проверки работы "{homework_name}". {verdict}'
-    return message
+    return (f'Изменился статус проверки работы "{homework_name}". {verdict}')
 
 
 def main():
@@ -94,25 +97,28 @@ def main():
 
     bot = Bot(token=TELEGRAM_TOKEN)
     send_message(bot, 'Запуск Бота')
-    current_timestamp = int(time.time())
+    timestamp = int(time.time())
     last_message_cache = ''
     while True:
         try:
-            response = get_api_answer(current_timestamp)
+            response = get_api_answer(timestamp)
             homeworks = check_response(response)
             for homework in homeworks:
                 message = parse_status(homework)
-                if message != '':
+                if message != last_message_cache:
                     send_message(bot, message)
-            current_timestamp = int(time.time())
-        except IndexError:
-            logging.error('Формат не соответсвует JSON')
+                    last_message_cache = message
+
+        except SendMessageError as error:
+            message_with_error = (
+                f'Ошибка при отправке сообщения: {error}'
+            )
         except Exception as error:
-            message = f'{error}'
-            logging.error(message)
-            if message != last_message_cache:
-                send_message(bot, message)
-                last_message_cache = message
+            message_with_error = f'Сбой в работе бота: {error}'
+            logging.error(message_with_error)
+            if message_with_error != last_message_cache:
+                send_message(bot, message_with_error)
+                last_message_cache = message_with_error
         finally:
             time.sleep(RETRY_PERIOD)
         logging.info(f'Сообщение {message} отправлено')
